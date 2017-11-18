fabee3c3787160c589c3679113d985b9ae232543
arades79 <arades79@gmail.com>
2017-11-18 12:33:06 -0500

    add template .ino files

---
+++ emitter.ino
@@ -0,0 +1,12 @@
+// this code takes the input to the serial termincal
+// and converts to binary packets before emitting
+// the binary via an attached LED
+
+
+void setup() {
+  serial.Begin(1200);
+}
+
+void loop() {
+
+}
---
+++ receiver.ino
@@ -0,0 +1,12 @@
+// this code will look for blinking lights and synchronize its timing
+// then receive a packet cronstructed from the emitter.ino code
+// finally reconstructing the data and sending it back through the terminal
+
+
+void setup() {
+  serial.Begin(1200);
+}
+
+void loop() {
+
+}
